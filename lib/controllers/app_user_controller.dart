503126a5837e2089b7bc4aaa1270c7000abb62b4
BarGreen <satanarchist69@gmail.com>
2023-04-04 20:20:19 +0300

    + passwordrefresh

--- lib/controllers/app_user_controller.dart
+++ lib/controllers/app_user_controller.dart
@@ -30,20 +30,56 @@ class AppUserController extends ResourceController {
   }
 
   @Operation.post()
-  Future<Response> updateProfile() async {
+  Future<Response> updateProfile(
+    @Bind.header(HttpHeaders.authorizationHeader) String header,
+    @Bind.body() User user,
+  ) async {
       try {
-        return AppResponse.ok(message: "updateProfile");
+        final id = AppUtils.getIdFromHeader(header);
+        final fUser = await managedContext.fetchObjectWithID<User>(id);
+        final qUpdateUser = Query<User>(managedContext)
+          ..where((x) => x.id).equalTo(id)
+          ..values.username = user.username?? fUser?.username
+          ..values.email = user.email?? fUser?.email;
+        await qUpdateUser.updateOne();
+        final uUser = await managedContext.fetchObjectWithID<User>(id);
+        uUser?.removePropertiesFromBackingMap(
+          [AppConst.accessToken, AppConst.refreshToken]);
+        return AppResponse.ok(
+          message: "Успешное обновление данных", 
+          body: uUser?.backing.contents);
       } catch (error) {
-        return AppResponse.serverError(error);
+        return AppResponse.serverError(error, 
+          message: "Ошибка обновление данных");
       }
     }
 
   @Operation.put()
-  Future<Response> updatePassword() async {
+  Future<Response> updatePassword(
+    @Bind.header(HttpHeaders.authorizationHeader) String header,
+    @Bind.query("oldPassword") String oldPassword,
+    @Bind.query("newPassword") String newPassword
+  ) async {
       try {
-        return AppResponse.ok(message: "updatePassword");
+        final id = AppUtils.getIdFromHeader(header);
+        final qFindUser = Query<User>(managedContext)
+          ..where((table) => table.id).equalTo(id)
+          ..returningProperties(
+            (table) => [table.salt, table.hashPassword]);
+        final findUser = await qFindUser.fetchOne();
+        final salt = findUser?.salt ?? "";
+        final oldPasswordHash = generatePasswordHash(oldPassword, salt);
+        if (oldPasswordHash != findUser?.hashPassword) {
+          return AppResponse.badRequest(message: "Пароль неверный");
+        }
+        final newPasswordHash = generatePasswordHash(newPassword, salt);
+        final qUpdateUser = Query<User>(managedContext)
+          ..where((x) => x.id).equalTo(id)
+          ..values.hashPassword = newPasswordHash;
+        await qUpdateUser.updateOne();
+        return AppResponse.ok(message: "Успешное обновление пароля");
       } catch (error) {
-        return AppResponse.serverError(error);
+        return AppResponse.serverError(error, message: "Ошибка обновления пароля");
       }
     }
 }
